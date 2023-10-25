851c7b61af9e9c7f889e9b74eabd43f5496f0a09
jsuazo <javier2587@gmail.com>
2023-10-25 11:09:04 -0300

    add check permission camera

--- app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jsuazo.horoscopeapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -13,6 +14,21 @@ class PalmistryFragment : Fragment() {
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (checkCameraPermission()) {
+
+        }
+    }
+
+    private fun checkCameraPermission(): Boolean {
+        return PermissionChecker.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        ) == PermissionChecker.PERMISSION_GRANTED
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
