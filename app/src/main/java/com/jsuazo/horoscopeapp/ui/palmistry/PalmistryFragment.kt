bf8a218154f3b8938018094e9211058297a07505
jsuazo <javier2587@gmail.com>
2023-10-25 11:14:59 -0300

    permissions management

--- app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jsuazo.horoscopeapp.databinding.FragmentPalmistryBinding
@@ -12,21 +15,38 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
+    companion object {
+        private const val CAMERA_PERMISSION = android.Manifest.permission.CAMERA
+    }
+
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) {
+        isGranted ->
+        if(isGranted) {
+
+        } else {
+            Toast.makeText(requireContext(), "No puedes continuar si no aceptas los permisos.", Toast.LENGTH_LONG).show()
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         if (checkCameraPermission()) {
 
+        } else {
+            requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
     }
 
     private fun checkCameraPermission(): Boolean {
         return PermissionChecker.checkSelfPermission(
             requireContext(),
-            android.Manifest.permission.CAMERA
+            CAMERA_PERMISSION
         ) == PermissionChecker.PERMISSION_GRANTED
     }
 
