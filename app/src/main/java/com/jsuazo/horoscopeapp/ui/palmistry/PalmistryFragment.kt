5461718b515447b09a3bb09c5259a4897205a470
jsuazo <javier2587@gmail.com>
2023-10-25 11:28:34 -0300

    start Camera function

--- app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/jsuazo/horoscopeapp/ui/palmistry/PalmistryFragment.kt
@@ -1,12 +1,16 @@
 package com.jsuazo.horoscopeapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.jsuazo.horoscopeapp.databinding.FragmentPalmistryBinding
@@ -27,17 +31,42 @@ class PalmistryFragment : Fragment() {
     ) {
         isGranted ->
         if(isGranted) {
-
+            startCamera()
         } else {
             Toast.makeText(requireContext(), "No puedes continuar si no aceptas los permisos.", Toast.LENGTH_LONG).show()
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider:ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }
+            catch (e: Exception) {
+                Log.e("ERROR CAMERA", "Error: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
